a1171d295613c5bc143461e7628c9fffc6c1f6bf
Jingyang Deng <jydeng0114@outlook.com>
2021-10-22 19:28:01 +0800

    add temp main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include <string>
-
-#include "utils/csv_reader.h"
-#include "utils/dataset.h"
+#include "utils/loader.h"
+#include "model/c4_5.h"
 
 int main() {
     std::string path = "./car-dataset/car.data";
-    Dataset* d = new Dataset(path, 0.3);
-    d->show();
+    std::vector<double> ratio = {0.6, 0.2, 0.2};
+    Loader loader(path, ratio);
+    DecisionTreeC4_5 tree;
+
+    tree.train(loader.train_ds);
+    tree.test(loader.test_ds);
     return 0;
 }
