1b9d0912cae3efdbdf9965f105ad98c4bc74785c
Jingyang Deng <jydeng0114@outlook.com>
2021-10-23 19:39:11 +0800

    update temp main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,14 @@ int main() {
     std::vector<double> ratio = {0.6, 0.2, 0.2};
     Loader loader(path, ratio);
 
+    std::cout << "-------------- C4.5 --------------" << std::endl;
+
     DecisionTreeC4_5 tree_c45;
     tree_c45.train(loader.train_ds, nullptr);
     tree_c45.test(loader.test_ds);
+
+    std::cout << std::endl;
+    std::cout << "-------------- CART --------------" << std::endl;
 
     DecisionTreeCART tree_cart;
     tree_cart.train(loader.train_ds, loader.val_ds);
